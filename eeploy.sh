91336e4bd209db0c2aac9f942a8e9d3806cdd90f
idk <idk@idk.local>
2020-02-12 20:54:51 -0500

    Add mirror, submodules

--- eeploy.sh
+++ eeploy.sh
@@ -9,16 +9,18 @@ CHRDOCROOT=/var/lib/i2p/i2p-config/eepsite/docroot/I2P-Configuration-For-Chromiu
 CHRDOCREPO=https://github.com/eyedeekay/I2P-Configuration-For-Chromium
 FNGDOCROOT=/var/lib/i2p/i2p-config/eepsite/docroot/I2P-Browser-Attackability-Evaluation/
 FNGDOCREPO=https://github.com/eyedeekay/I2P-Browser-Attackability-Evaluation
+MIRDOCROOT=/var/lib/i2p/i2p-config/eepsite/docroot/mirror/
+MIRDOCREPO=https://github.com/eyedeekay/mirror
+
 
 function setuppage(){
     DOCROOT=$1
     DOCREPO=$2
     echo "Setting personal site from $DOCREPO in $DOCROOT"
     if [ -d "$DOCROOT" ]; then
-        echo "$DOCROOT was found"
         if [ -d "$DOCROOT.git" ]; then
-            echo "$DOCROOT is a git repository"
             cd "$DOCROOT" && pwd && git pull --all
+            cd "$DOCROOT" && git submodule update --recursive --remote
         fi
     else
         git clone "$DOCREPO" "$DOCROOT"
@@ -29,3 +31,4 @@ setuppage $IDKDOCROOT $IDKDOCREPO
 setuppage $FFXDOCROOT $FFXDOCREPO
 setuppage $CHRDOCROOT $CHRDOCREPO
 setuppage $FNGDOCROOT $FNGDOCREPO
+setuppage $MIRDOCROOT $MIRDOCREPO
