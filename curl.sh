4ced6831dc20f65cd7787d1c1982a92e9daa4824
idk <idk@laptop.idk.i2p>
2020-10-11 19:08:45 -0400

    work on the seeder script

--- curl.sh
+++ curl.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
+rm -rfv ~/.i2p/i2psnark/idk.i2p ~/.i2p/i2psnark/idk.i2p.torrent ~/.i2p/i2psnark/idk.i2p.zip
 wget -O ~/.i2p/i2psnark/idk.i2p.torrent https://eyedeekay.github.io/idk.i2p.torrent
 wget -O ~/.i2p/i2psnark/idk.i2p.zip https://github.com/eyedeekay/eyedeekay.github.io/archive/master.zip
-unzip -j ~/.i2p/i2psnark/idk.i2p.zip -d ~/.i2p/i2psnark/idk.i2p
+unzip ~/.i2p/i2psnark/idk.i2p.zip -d ~/.i2p/i2psnark/
+mv ~/.i2p/i2psnark/eyedeekay.github.io-master ~/.i2p/i2psnark/idk.i2p
