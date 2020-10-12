12825c3745512a957d9a8783157b4c7ab94d3b61
idk <idk@laptop.idk.i2p>
2020-10-11 20:39:28 -0400

    work on the seeding script

--- curl.sh
+++ curl.sh
@@ -3,4 +3,3 @@ rm -rfv ~/.i2p/i2psnark/idk.i2p ~/.i2p/i2psnark/idk.i2p.torrent ~/.i2p/i2psnark/
 wget -O ~/.i2p/i2psnark/idk.i2p.zip https://github.com/eyedeekay/eyedeekay.github.io/releases/download/current/idk.i2p.zip
 wget -O ~/.i2p/i2psnark/idk.i2p.torrent https://eyedeekay.github.io/idk.i2p.torrent
 unzip ~/.i2p/i2psnark/idk.i2p.zip -d ~/.i2p/i2psnark/
-mv ~/.i2p/i2psnark/eyedeekay.github.io-master ~/.i2p/i2psnark/idk.i2p
