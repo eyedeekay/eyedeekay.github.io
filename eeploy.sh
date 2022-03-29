23aae0aeccbe78f571eeb002e7422476aef75c27
idk <hankhill19580@gmail.com>
2022-03-29 00:59:52 -0400

    update

--- eeploy.sh
+++ eeploy.sh
@@ -9,7 +9,6 @@ fi
 #Don't forget the trailing slash on all directories or the site will not update.
 IDKDOCROOT="$CONFIGDOCROOT"eepsite/docroot/
 IDKDOCREPO=https://github.com/eyedeekay/eyedeekay.github.io
-
 FFXDOCROOT="$CONFIGDOCROOT"eepsite/docroot/I2P-in-Private-Browsing-Mode-Firefox/
 FFXDOCREPO=https://github.com/eyedeekay/I2P-in-Private-Browsing-Mode-Firefox
 FFXSTATROOT="$CONFIGDOCROOT"eepsite/docroot/firefox-static/
@@ -20,69 +19,51 @@ FNGDOCROOT="$CONFIGDOCROOT"eepsite/docroot/I2P-Browser-Attackability-Evaluation/
 FNGDOCREPO=https://github.com/eyedeekay/I2P-Browser-Attackability-Evaluation
 MIRDOCROOT="$CONFIGDOCROOT"eepsite/docroot/mirror/
 MIRDOCREPO=https://github.com/eyedeekay/mirror
-
 JAVADOCROOT="$CONFIGDOCROOT"eepsite/docroot/javadoc-i2p/
 JAVADOCREPO=https://github.com/eyedeekay/javadoc-i2p
-
 BRBDOCROOT="$CONFIGDOCROOT"eepsite/docroot/brb/
 BRBDOCREPO=https://github.com/eyedeekay/brb
-
 MTGDOCROOT="$CONFIGDOCROOT"eepsite/docroot/mtg-i2p/
 MTGDOCREPO=https://github.com/eyedeekay/mtg-i2p
-
 JPAKDOCROOT="$CONFIGDOCROOT"eepsite/docroot/i2p/
 JPAKDOCREPO=https://github.com/eyedeekay/i2p
-
 EEPDOCROOT="$CONFIGDOCROOT"eepsite/docroot/eephttpd/
 EEPDOCREPO=https://github.com/eyedeekay/eephttpd
-
 BRBDOCROOT="$CONFIGDOCROOT"eepsite/docroot/brb/
 BRBDOCREPO=https://github.com/eyedeekay/brb
-
 IRCDOCROOT="$CONFIGDOCROOT"eepsite/docroot/terrarium/
 IRCDOCREPO=https://github.com/eyedeekay/terrarium
-
 RAILDOCROOT="$CONFIGDOCROOT"eepsite/docroot/railroad/
 RAILDOCREPO=https://github.com/eyedeekay/railroad
-
 RESEEDDOCROOT="$CONFIGDOCROOT"eepsite/docroot/reseed-tools/
 RESEEDDOCREPO=https://github.com/eyedeekay/reseed-tools
-
 DOKUDOCROOT="$CONFIGDOCROOT"eepsite/docroot/Dokuwiki-over-I2P/
 DOKUDOCREPO=https://github.com/eyedeekay/Dokuwiki-over-I2P
-
 NEXTDOCROOT="$CONFIGDOCROOT"eepsite/docroot/Nextcloud-over-I2P-on-Docker/
 NEXTDOCREPO=https://github.com/eyedeekay/Nextcloud-over-I2P-on-Docker
-
 SSHDOCROOT="$CONFIGDOCROOT"eepsite/docroot/i2p-i2pd-sshsetup/
 SSHDOCREPO=https://github.com/eyedeekay/i2p-i2pd-sshsetup
-
 BLIZZARDDOCROOT="$CONFIGDOCROOT"eepsite/docroot/blizzard/
 BLIZZARDDOCREPO=https://github.com/eyedeekay/blizzard
-
 WESNOTHDOCROOT="$CONFIGDOCROOT"eepsite/docroot/battle-for-wesnoth-i2p/
 WESNOTHDOCREPO=https://github.com/eyedeekay/battle-for-wesnoth-i2p
-
 NETHACKDOCROOT="$CONFIGDOCROOT"eepsite/docroot/nethack-i2p/
 NETHACKDOCREPO=https://github.com/eyedeekay/nethack-i2p
-
 THUNDERBIRDDOCROOT="$CONFIGDOCROOT"eepsite/docroot/Thunderbird-I2P-Auto-Configuration/
 THUNDERBIRDDOCREPO=https://github.com/eyedeekay/Thunderbird-I2P-Auto-Configuration
-
 DUNGEONQUESTDOCROOT="$CONFIGDOCROOT"eepsite/docroot/dungeonQuest/
 DUNGEONQUESTDOCREPO=https://github.com/eyedeekay/dungeonQuest
-
 DEBDOCROOT="$CONFIGDOCROOT"eepsite/docroot/apt/
 DEBDOCREPO=https://github.com/eyedeekay/apt
-
 FDROIDDOCROOT="$CONFIGDOCROOT"eepsite/docroot/fdroid/
 FDROIDDOCREPO=https://github.com/eyedeekay/fdroid
-
 FDROIDDEVDOCROOT="$CONFIGDOCROOT"eepsite/docroot/fdroid-dev/
 FDROIDDEVDOCREPO=https://github.com/eyedeekay/fdroid-dev
-
 TORMANAGERDOCROOT="$CONFIGDOCROOT"eepsite/docroot/i2p.plugins.tor-manager/
 TORMANAGERDOCREPO=https://github.com/eyedeekay/i2p.plugins.tor-manager
+TORMIRRRORDOCROOT="$CONFIGDOCROOT"eepsite/docroot/torbrowser/
+TORMIRRORDOCREPO=https://github.com/eyedeekay/torbrowser
+
 
 function setuppage(){
     DOCROOT=$1
@@ -114,11 +95,6 @@ function pluginsetup(){
     make download-su3s
 }
 
-pluginsetup $RAILDOCROOT $RAILDOCREPO
-pluginsetup $BLIZZARDDOCROOT $BLIZZARDDOCREPO
-pluginsetup $MTGDOCROOT $MTGDOCREPO
-pluginsetup $THUNDERBIRDDOCROOT $THUNDERBIRDDOCREPO
-pluginsetup $DUNGEONQUESTDOCROOT $DUNGEONQUESTDOCREPO
 setuppage $NETHACKDOCROOT $NETHACKDOCREPO
 setuppage $WESNOTHDOCROOT $WESNOTHDOCREPO
 setuppage $IDKDOCROOT $IDKDOCREPO
@@ -136,6 +112,13 @@ setuppage $JAVADOCROOT $JAVADOCREPO
 setuppage $FDROIDDOCROOT $FDROIDDOCREPO
 setuppage $DEBDOCROOT $DEBDOCREPO
 setuppage $FDROIDDEVDOCROOT $FDROIDDEVDOCREPO
+setuppage $TORMIRRRORDOCROOT $TORMIRRORDOCREPO
+
+pluginsetup $RAILDOCROOT $RAILDOCREPO
+pluginsetup $BLIZZARDDOCROOT $BLIZZARDDOCREPO
+pluginsetup $MTGDOCROOT $MTGDOCREPO
+pluginsetup $THUNDERBIRDDOCROOT $THUNDERBIRDDOCREPO
+pluginsetup $DUNGEONQUESTDOCROOT $DUNGEONQUESTDOCREPO
 pluginsetup $RESEEDDOCROOT $RESEEDDOCREPO
 pluginsetup $TORMANAGERDOCROOT $TORMANAGERDOCREPO
 pluginsetup $IRCDOCROOT $IRCDOCREPO
